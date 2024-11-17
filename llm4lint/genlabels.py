9ccf181133019978fbb0cf9e493cace46db63211
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-11-17 19:55:11 +0530

    refactor genlabels.py: change linter function to be cleaner

--- llm4lint/genlabels.py
+++ llm4lint/genlabels.py
@@ -3,21 +3,21 @@ import sys
 from pathlib import Path
 import subprocess
 
-def lint_dataset(linter: Callable[[Path], bytes], dataset_path: Path = Path("../stack-v2-smol")):
+def lint_dataset(linter: Callable[[Path], str], dataset_path: Path = Path("../stack-v2-smol")):
     """performs linting on the entire dataset,
     and saves the output in a directory with same filenames of source codes
     """
-    # LABEL_ROOT = "../labels"
-    # Path(LABEL_ROOT).mkdir(exist_ok=True)
     for repo_owner in dataset_path.iterdir():
         project = next(repo_owner.iterdir())
         for file in project.iterdir():
-            label = linter(file).decode(encoding=sys.stdout.encoding)
+            label = linter(file)
             output_file = file.with_suffix(".txt")
             with open(output_file, "w", encoding="utf-8") as f:
                 f.write(label)
-            break
 
-
-linter = lambda file: subprocess.run(['pylint', '--persistent=n', '--output-format=json', file], capture_output=True).stdout
-lint_dataset(linter)
+def linter_pylint(file: Path) -> str:
+    """performs linting on file using pylint, returns output as json"""
+    out = subprocess.run(['pylint', '--persistent=n', '--output-format=json', file],
+                         capture_output=True).stdout
+    return out.decode(encoding=sys.stdout.encoding)
+lint_dataset(linter=linter_pylint)
