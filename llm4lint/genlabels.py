0af03f854d61f5c42aeb488f96bf682a50f9e3c0
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-11-18 21:30:08 +0530

    parse pylint json output to find all messages

--- llm4lint/genlabels.py
+++ llm4lint/genlabels.py
@@ -2,6 +2,7 @@ from typing import Callable
 import sys
 from pathlib import Path
 import subprocess
+import json
 import pandas as pd
 
 def lint_dataset(linter: Callable[[Path], str], dataset_path: Path = Path("../stack-v2-smol"), save_path: Path = Path("../datasets/dataset_pylint.csv")) -> pd.DataFrame:
@@ -16,18 +17,33 @@ def lint_dataset(linter: Callable[[Path], str], dataset_path: Path = Path("../st
             with open(file, "r", encoding="utf-8") as code:
                 dataset["code"].append(code.read())
             dataset["label"].append(label)
-            output_file = file.with_suffix(".json")
-            with open(output_file, "w", encoding="utf-8") as f:
-                f.write(label)
+            # save json output if raw linter function is used.
+            # This part is unnecessary though, no longer used
+            if linter.__name__ == "linter_pylint_raw":
+                output_file = file.with_suffix(".json")
+                with open(output_file, "w", encoding="utf-8") as f:
+                    f.write(label)
     df = pd.DataFrame(dataset)
     save_path.parent.mkdir(exist_ok=True, parents=True)
     df.to_csv(save_path, index=False, encoding="utf-8")
     return df
 
-def linter_pylint(file: Path) -> str:
+def linter_pylint_raw(file: Path) -> str:
     """performs linting on file using pylint, returns output as json"""
     out = subprocess.run(['pylint', '--persistent=n', '--output-format=json', file],
                          capture_output=True).stdout
     return out.decode(encoding=sys.stdout.encoding)
+
+def linter_pylint(file: Path) -> str:
+    """performs linting on file using pylint, returns source code issues messages"""
+    raw_out = subprocess.run(['pylint', '--persistent=n', '--output-format=json', file],
+                         capture_output=True).stdout
+    raw_out = raw_out.decode(encoding=sys.stdout.encoding)
+    json_objs = json.loads(raw_out)
+    messages = ""
+    for obj in json_objs:
+        message = str(obj["line"]) + " " + obj["message"] + "\n"
+        messages += message
+    return messages
 
 lint_dataset(linter=linter_pylint)
