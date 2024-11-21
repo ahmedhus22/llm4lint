8c1958650c2375f3c9711546dc629107f375f535
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-11-22 00:29:31 +0530

    update stackv2 dl script to download fixed no of python files

--- llm4lint/stackv2.py
+++ llm4lint/stackv2.py
@@ -9,8 +9,8 @@ import datasets.config
 
 
 s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
-download_path = ".."
-datasets.config.DOWNLOADED_DATASETS_PATH = Path(download_path)
+DOWNLOAD_PATH = ".."
+datasets.config.DOWNLOADED_DATASETS_PATH = Path(DOWNLOAD_PATH)
 
 def download_contents(files):
     for file in files:
@@ -19,17 +19,22 @@ def download_contents(files):
             file["content"] = fin.read().decode(file["src_encoding"])
     return {"files": files}
 
-ds = load_dataset("bigcode/the-stack-v2-train-smol-ids", split="train", streaming=True, token=os.environ["HF_TOKEN"], cache_dir=download_path)
+ds = load_dataset("bigcode/the-stack-v2-train-smol-ids", split="train", streaming=True, token=os.environ["HF_TOKEN"], cache_dir=DOWNLOAD_PATH)
 ds = ds.map(lambda row: download_contents(row["files"]))
+
 DOWNLOAD_ROOT = "../stack-v2-smol"
+DATASET_SIZE = int(1e+3)
+no_python_files = 0
 for row in ds:
     repo_name = row["repo_name"]
-    Path(DOWNLOAD_ROOT, repo_name).mkdir(exist_ok=True, parents=True)
     for file in row["files"]:
         blob_id = file["blob_id"]
         content = file["content"]
         if file["language"] == "Python":
+            Path(DOWNLOAD_ROOT, repo_name).mkdir(exist_ok=True, parents=True)
             data_path = Path(DOWNLOAD_ROOT, repo_name, blob_id + ".py")
             with open(data_path, "w", encoding="utf-8") as f:
                 f.write(content)
-    break # REMOVE THIS BREAK TO DOWNLOAD THE COMPLETE DATASET
+            no_python_files += 1
+    if no_python_files == DATASET_SIZE:
+        break
