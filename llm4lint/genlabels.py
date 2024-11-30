fa5078f4c2506c3c3aede6e4b52dc75fd1582cb1
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-11-30 20:00:09 +0530

    modify lints: disable pylint module name msg, add msg type to lint

--- llm4lint/genlabels.py
+++ llm4lint/genlabels.py
@@ -58,13 +58,14 @@ def linter_pylint(file: Union[Path, List]) -> str:
 
 def linter_pylint_project(files: List) -> List:
     """performs linting on all files using pylint, returns dictionary of source code issues messages"""
-    raw_out = subprocess.run(['pylint', '--persistent=n', '--disable=import-error', '--output-format=json'] + files,
+    raw_out = subprocess.run(['pylint', '--persistent=n', '--disable=import-error,C0103', '--output-format=json'] + files,
                          capture_output=True).stdout
     raw_out = raw_out.decode(encoding=sys.stdout.encoding)
     json_objs = json.loads(raw_out)
     messages = {}
     for obj in json_objs:
-        message = str(obj["line"]) + " " + obj["message"] + "\n"
+        message = str(obj["line"]) + " - " + obj["type"] + ": " + obj["message"] + "\n" #" (" + obj["symbol"].replace("-", " ") + ")" + "\n"
+        print(message)
         if not obj["module"] in messages.keys():
             messages[obj["module"]] = ""
         messages[obj["module"]] += (message)
