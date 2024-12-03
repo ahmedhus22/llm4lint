a7bb71aa7c658e37419c22dadd422ad0ef8c7465
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-12-03 12:32:41 +0000

    optimze stackv2 download by skipping files that already exist

--- llm4lint/stackv2.py
+++ llm4lint/stackv2.py
@@ -12,8 +12,13 @@ s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
 DOWNLOAD_PATH = ".."
 datasets.config.DOWNLOADED_DATASETS_PATH = Path(DOWNLOAD_PATH)
 
-def download_contents(files):
+def download_contents(files, repo_name):
+    # UNCOMMENT TO SKIP ENTIRE REPO
+    # if Path(DOWNLOAD_ROOT, repo_name).exists():
+    #     return {"files": None}
     for file in files:
+        if Path(DOWNLOAD_ROOT, repo_name, file['blob_id'] + ".py").exists():
+            continue
         if file["language"] != "Python":
             continue
         s3_url = f"s3://softwareheritage/content/{file['blob_id']}"
@@ -22,14 +27,19 @@ def download_contents(files):
     return {"files": files}
 
 ds = load_dataset("bigcode/the-stack-v2-train-smol-ids", split="train", streaming=True, token=os.environ["HF_TOKEN"], cache_dir=DOWNLOAD_PATH)
-ds = ds.map(lambda row: download_contents(row["files"]))
+ds = ds.map(lambda row: download_contents(row["files"], row["repo_name"]))
 
 DOWNLOAD_ROOT = "../stack-v2-smol"
 DATASET_SIZE = int(1e+3)
 no_python_files = 0
 for row in ds:
     repo_name = row["repo_name"]
+    # UNCOMMENT TO SKIP ENTIRE REPO
+    # if Path(DOWNLOAD_ROOT, repo_name).exists():
+    #     continue
     for file in row["files"]:
+        if Path(DOWNLOAD_ROOT, repo_name, file["blob_id"] + ".py").exists():
+            continue
         if file["language"] == "Python":
             blob_id = file["blob_id"]
             content = file["content"]
