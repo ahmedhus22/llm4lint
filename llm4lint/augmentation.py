c04ad8730db476bab3562b668c073063804ab286
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-12-05 17:49:47 +0000

    add augmentation module to modify src code using ast

---
+++ llm4lint/augmentation.py
@@ -0,0 +1,46 @@
+"""augmentation module generates more data by modifying syntax tree"""
+import ast
+
+example_code = """
+l = [1,2,3,4]
+for i in l:
+    l.append(1)
+"""
+
+
+class FindNames(ast.NodeVisitor):
+    """finds all unique name ids: 
+    call FindNamesobj.visit(tree) to store names in FindNamesobj.var_nodes atrr"""
+    def __init__(self) -> None:
+        super().__init__()
+        self.var_nodes = set()
+
+    def visit_Name(self, node):
+        #print(node.id)
+        self.var_nodes.add(node.id)
+        return self.var_nodes
+
+
+class NameNodeTransformer(ast.NodeTransformer):
+    """Transforms all instances of old_name node to new_name node"""
+    def __init__(self, old_name: ast.Name, new_name: ast.Name) -> None:
+        super().__init__()
+        self.old_name = old_name
+        self.new_name = new_name
+
+    def visit_Name(self, node):
+        if node.id == self.old_name.id:
+            # print(node.id, "changed to", self.new_name.id)
+            return ast.Name(id=self.new_name.id, ctx=node.ctx)
+        return node
+    
+tree = ast.parse(example_code)
+name_l = ast.Name(id='l')
+new_name = ast.Name(id='test')
+new_tree = ast.fix_missing_locations(NameNodeTransformer(old_name=name_l, new_name=new_name).visit(tree))
+print(ast.unparse(new_tree))
+
+# tree = ast.parse(example_code)
+# var_name_visitor = FindNames()
+# print(var_name_visitor.visit(tree))
+# print(var_name_visitor.var_nodes)
