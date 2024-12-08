b1476d2a0e9d809cc795ce1dbd700d32bef8e144
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-12-08 15:09:23 +0000

    train: add feat to pass no of parameters to train that model

--- llm4lint/train.py
+++ llm4lint/train.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 from llm import LLM, InstructLLM
 from augmentation import augment_dataset
@@ -8,18 +9,21 @@ def train(model_name, model_path: Path = None):
         model_name,
         model_path=model_path
     )
-    qwen_base.train(Path("../datasets/examples.csv"))
+    qwen_base.train(Path("../datasets/dataset_combined.csv"))
     qwen_base.save(Path("../models/q4_gguf"), format="q4_gguf")
 
 def main():
-    AUGMENT_DATA = True
-    if AUGMENT_DATA:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--augment_data", help="specify filename to perform augmentation")
+    parser.add_argument("-p", "--parameters", choices=["1.5b", "3b"], help="Qwen2.5-Coder Model Parameters", required=True)
+    args = parser.parse_args()
+    if not args.augment_data is None:
         augment_dataset(
             Path("../datasets/examples.csv"),
-            save_path=Path("../datasets/examples_aug.csv"),
+            save_path=Path("../datasets/trainer_aug.csv"),
             save_format="csv"
         )
-    train("unsloth/Qwen2.5-Coder-1.5B-bnb-4bit")
+    train("unsloth/Qwen2.5-Coder-" + str.upper(args.parameters) + "-bnb-4bit")
 
 if __name__=="__main__":
     main()
