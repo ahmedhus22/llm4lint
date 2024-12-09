6b2f59cbb799cfa5ee40f2ad318d744e8bd56bc1
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-12-09 12:27:36 +0000

    implement ollama inference for cli app

--- llm4lint/llm4lint.py
+++ llm4lint/llm4lint.py
@@ -3,16 +3,24 @@ from typing import Iterator
 from pathlib import Path
 from ollama import chat, ChatResponse
 
+from augmentation import _addcodelines
+
 class App:
     def __init__(self, model: str) -> None:
         self.model:str = model
-        self.lint_prompt = "Perform linting on the given code. Specify output in format: <line_number> - <type>: <issue>"
+        self.lint_prompt = "Perform linting on the given code. Specify output in format: <line_number> - <type>: <issue>\n"
+
+    def _getcode(self, path: Path) -> str:
+        with open(path, "r", encoding="utf-8") as f:
+            code: str = f.read()
+        code = _addcodelines(code)
+        return code
 
     def get_lints(self, file: Path) -> Iterator[ChatResponse]:
         """returns predicted tokens as a stream(iterable),
         chunk["message"]["content"]"""
-        with open(file, "r", encoding="utf-8") as src_file:
-            user_code = src_file.read()
+        user_code = self._getcode(file)
+        #print(user_code + "\n" + "Analyzing...")
         stream = chat(
             model=self.model,
             messages=[{'role': 'user', 'content': self.lint_prompt + user_code}],
@@ -37,10 +45,10 @@ def main():
     parser.add_argument("--examples", default=None, help="csv file with linting examples: cols=input, output")
     parser.add_argument("--interactive", action="store_true", help=App.init_shell.__doc__)
     args = parser.parse_args()
-    # cli_app = App()
+    cli_app = App("llm4lint")
+    stream = cli_app.get_lints(args.filename)
+    for chunk in stream:
+        print(chunk['message']['content'], end='', flush=True)
 
 if __name__=="__main__":
     main()
-
-# for chunk in stream:
-#   print(chunk['message']['content'], end='', flush=True)
