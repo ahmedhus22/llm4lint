706e39ed1998137538c12e664dffa6e8cb679f00
Syed Ahmed Hussain <ahmedhus22@gmail.com>
2024-12-09 13:03:54 +0000

    add 7b and 14b parameter model options for training

--- llm4lint/train.py
+++ llm4lint/train.py
@@ -7,7 +7,7 @@ from datasets import load_dataset
 def train(model_name, model_path: Path = None):
     qwen_base = LLM(
         model_name,
-        model_path=model_path
+        model_path=model_path,
     )
     qwen_base.train(Path("../datasets/dataset_combined.csv"))
     qwen_base.save(Path("../models/q4_gguf"), format="q4_gguf")
@@ -15,7 +15,7 @@ def train(model_name, model_path: Path = None):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--augment_data", help="specify filename to perform augmentation")
-    parser.add_argument("-p", "--parameters", choices=["1.5b", "3b"], help="Qwen2.5-Coder Model Parameters", required=True)
+    parser.add_argument("-p", "--parameters", choices=["1.5b", "3b", "7b", "14b"], help="Qwen2.5-Coder Model Parameters", required=True)
     args = parser.parse_args()
     if not args.augment_data is None:
         augment_dataset(
